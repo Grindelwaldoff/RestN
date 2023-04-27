350abca9f1575ed2116a51c27b235c5e80117885
ubuntuUser <vsevolodrybnik@gmail.com>
2023-04-27 20:53:53 +0300

    hide secret key

--- CatApi/CatApi/settings.py
+++ CatApi/CatApi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u*-jq-x4msfw2b38custmrgep*2f*=9ynumyg2mo+-z5f-chu#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
